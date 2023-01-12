d0c671d6906cf7efa34d97311747f519540c6c7d
jfd02 <jerry@jerrydurkin.com>
2023-01-11 19:30:48 -0500

    Home sections for collection view

---
+++ FetchDessert/FetchDessert/View Controllers/Home/HomeSections.swift
@@ -0,0 +1,15 @@
+//
+//  HomeSections.swift
+//  FetchDessert
+//
+//  Created by Jerry Durkin on 1/11/23.
+//
+
+enum HomeSections: Int {
+    case header = 0
+    case search = 1
+    case desserts = 2
+    
+    private static let allValues = [header, search, desserts]
+    static let count = allValues.count
+}
