d77d2d7249c073147c613e328d4ea17054b790a4
jfd02 <jerry@jerrydurkin.com>
2023-01-11 19:31:03 -0500

    Remote image view with fade in

--- FetchDessert/FetchDessert/Helpers/RemoteImageView.swift
+++ FetchDessert/FetchDessert/Helpers/RemoteImageView.swift
@@ -11,13 +11,16 @@ import Combine
 class RemoteImageView: UIImageView {
     func load(url: URL, completion: @escaping (_ image: UIImage) -> Void) {
         DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url) {
-                if let image = UIImage(data: data) {
-                    DispatchQueue.main.async {
-                        self?.image = image
-                        completion(image)
-                    }
-                }
+            guard let data = try? Data(contentsOf: url) else { return }
+            guard let image = UIImage(data: data) else { return }
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                UIView.transition(with: self,
+                              duration:0.5,
+                              options: .transitionCrossDissolve,
+                              animations: { self.image = image },
+                              completion: nil)
+                completion(image)
             }
         }
     }
